2280065b4bafffeba39963d1084987535144166d
王强 <wangqiang02@wondersgroup.com>
2016-07-19 22:23:19 +0800

    improved

--- src/test/groovy/com/wondersgroup/springdatajpa/sample/repository/UserRepositoryTest.java
+++ src/test/groovy/com/wondersgroup/springdatajpa/sample/repository/UserRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.wondersgroup.springdatajpa.sample.repository;
 
+import com.apple.eawt.AppEvent;
 import com.wondersgroup.springdatajpa.sample.JpaRepositoryFactoryUtils;
 import com.wondersgroup.springdatajpa.sample.entity.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,12 +35,18 @@ public class UserRepositoryTest {
     @Test
     public void testFinaAll() throws Exception {
 
-        Map<String, Object> filters = new HashMap<>();
+        Map<String, Object> filters = new LinkedHashMap<>();
         filters.put("username_notLike", "qiang");
         filters.put("password_equal", "123456");
-        filters.put("group_fetch", null);
         filters.put("age_gt", 22);
+        filters.put("group.name_equal", "group");
+        filters.put("group.description_notEqual", "desc");
+        filters.put("group_fetch", null);
 
-        userRepository.findAll(filters, null);
+        Page<User> page = userRepository.findAll(filters, null);
+        List<User> users = page.getContent();
+        for (User user : users) {
+            System.out.println("user.getUsername() = " + user.getUsername());
+        }
     }
 }
